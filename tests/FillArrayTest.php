bd8ba76227c0883c4fbc3b5388f6c1738ea620ea
Yakov Medvedev <yakovmedvedev@gmail.com>
2025-07-22 16:54:02 +0300

    Done with FillArray-test and func

--- tests/FillArrayTest.php
+++ tests/FillArrayTest.php
@@ -26,6 +26,7 @@ class FillArrayTest extends TestCase
         $value = '*';
         $start = 0;
         $end = count($coll);
+        
         $this->assertEquals(['*', '*', '*', '*'], fill($coll, $value, $start, $end));
         $value = '**';
         $start = 4;
@@ -36,15 +37,52 @@ class FillArrayTest extends TestCase
         $this->assertEquals(['*', '*', '*', '*'], fill($coll, $value, $start, $end));
         $end = 7;
         $this->assertEquals(['*', '*', '*', '**'], fill($coll, $value, $start, $end));
-        $start = null;
-        $this->assertEquals(['**', '**', '**', '**'], fill($coll, $value, $start, $end));
+        $this->assertEquals(['**', '**', '**', '**'], fill($coll, $value));
         $coll = [];
         $this->assertEquals([], fill($coll, $value, $start, $end));
     }
-    //     public function testFillFullDefault()
-    // {
-    //     $coll = [1, 2, 3, 4];
-    //     fill($coll, '*');
-    //     $this->assertEquals(['*', '*', '*', '*'], $coll);
-    // }
 }
+
+
+//tutor's
+//  BEGIN
+// class SolutionTest extends TestCase
+// {
+//     private $coll;
+
+//     public function setUp(): void
+//     {
+//         $this->coll = [1, 2, 3, 4];
+//     }
+
+//     public function testFill1(): void
+//     {
+//         fill($this->coll, '*', 1, 3);
+//         $this->assertEquals([1, '*', '*', 4], $this->coll);
+//     }
+
+//     public function testFill2(): void
+//     {
+//         fill($this->coll, '*');
+//         $this->assertEquals(['*', '*', '*', '*'], $this->coll);
+//     }
+
+//     public function testFill3(): void
+//     {
+//         fill($this->coll, '*', 10, 12);
+//         $this->assertEquals([1, 2, 3, 4], $this->coll);
+//     }
+
+//     public function testFill4(): void
+//     {
+//         fill($this->coll, '*', 2, 2);
+//         $this->assertEquals([1, 2, 3, 4], $this->coll);
+//     }
+
+//     public function testFill5(): void
+//     {
+//         fill($this->coll, '*', 0, 10);
+//         $this->assertEquals(['*', '*', '*', '*'], $this->coll);
+//     }
+// }
+// END
