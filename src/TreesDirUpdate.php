8e0ab9f826d020909634553047d6f16b97046f82
Yakov Medvedev <yakovmedvedev@gmail.com>
2025-08-27 19:18:36 +0300

    Trees Update Dir

--- src/TreesDirUpdate.php
+++ src/TreesDirUpdate.php
@@ -25,43 +25,28 @@ $tree = mkdir(
         mkdir('presentations')
     ]
 );
-// print_r($tree);
-print_r(getMeta($tree['children'][1]));
-print_r($tree['children'][1]['meta']);
-print_r(getMeta($tree['children'][2]));
-// print_r($tree['children']);
+// print_r($tree['children']['name']);
 
-function compressImages()
+function compressImages($tree)
 {
-    $children = getChildren($tree);
-    $newChildren = array_map(function ($child) {
-        $meta = getMeta($child);
-        if (isFile($child)) {
-                return getMeta($child)/2;
-        }
-        return $child;
-    }, $children);
-    return $newChildren;
+$children = getChildren($tree);
+$newChildren = array_map(function ($child) {
+    $name = getName($child);
+    if (isFile($child) && strpos(getName($child), '.jpg') === true) {
+            foreach ($child as $value) {
+            $meta = getMeta($child);
+            $meta['size'] = $meta['size'] / 2;
+            }
+        return mkfile($name, $meta);
+    }
+    return mkdir($name, getChildren($child), getMeta($child));
+    
+}, $children);
+
+return mkdir(getName($tree), $newChildren, getMeta($tree));
+
 }
-
-// $tree = mkdir('/', [
-//     mkfile('oNe'),
-//     mkfile('Two'),
-//     mkdir('THREE'),
-// ]);
-// print_r($tree);
-
-// $children = getChildren($tree);
-// $newChildren = array_map(function ($child) {
-//     $name = getName($child);
-//     if (isDirectory($child)) {
-//         return mkdir(strtolower($name), getChildren($child), getMeta($child));
-//     }
-//     return mkfile(strtolower($name), getMeta($child));
-// }, $children);
-
-// $tree2 = mkdir(getName($tree), $newChildren, getMeta($tree));
-// print_r($tree2);
+print_r(compressImages($tree));
 
 // Реализуйте функцию compressImages(), которая принимает на вход директорию, находит внутри нее картинки и "сжимает" их. Под сжиманием понимается уменьшение свойства size в метаданных в два раза. Функция должна вернуть обновленную директорию со сжатыми картинками и всеми остальными данными, которые были внутри этой директории.
 // Картинками считаются все файлы заканчивающиеся на .jpg.
