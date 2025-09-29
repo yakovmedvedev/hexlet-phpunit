9ed5efd7e095299787e7a28bae00fa67fa5f8773
Yakov Medvedev <yakovmedvedev@gmail.com>
2025-09-29 18:47:39 +0300

    Classes - Method

--- src/ClassesMethodMakeRational.php
+++ src/ClassesMethodMakeRational.php
@@ -9,6 +9,8 @@ class Rational
     {
         $this->numer = $numer;
         $this->denom = $denom;
+//        $this->numer2 = $numer2;
+//        $this->denom2 = $denom2;
     }
     public function getNumer()
     {
@@ -27,72 +29,81 @@ class Rational
     }
         return $this->numer = abs($this->numer);
     }
-    public function lcm()
+    function lcm(int $x, int $y): int
     {
-        return ($this->getNumer() * $this->getDenom() / $this->gcd());
+        return ($x * $y) / gcd($x, $y);
     }
+//    public function add($other) {
+//        $newNumer = $this->getNumer() * $other->getDenom() + $other->getNumer() * $this->getDenom();
+//        $newDenom = $this->getDenom() * $other->getDenom();
+//        return new Rational($newNumer, $newDenom);
+//    }
+     function add($other)
+ {
+     $commonDenom = lcm($this->getDenom(), $other->getDenom());
+     $numer1 = $this->getNumer() * ($commonDenom / $this->getDenom());
+     $numer2 = $other->getNumer() * ($commonDenom / $other->getDenom());
+     $resultNum = $numer1 + $numer2;
+     return makeRational($resultNum, $commonDenom);
+ }
 
 }
-$rat1 = new Rational(3, 5);
-$rat2 = new Rational(8, 9);
-//print_r($rat1->gcd() . "\n");
-print_r($rat1->lcm());
+$rat1 = new Rational(3, 6);
+print_r($rat1);
 print_r("\n");
-print_r($rat2->lcm());
-// function gcd(int $x, int $y): int
-// {
-//     while ($y != 0) {
-//         $temp = $y;
-//         $y = $x % $y;
-//         $x = $temp;
-//     }
-//     return abs($x);
-// }
+$rat2 = new Rational(2, 9);
+print_r($rat2);
+print_r("\n");
+////print_r($rat1->gcd() . "\n");
+//print_r("rat1 lcm " . $rat1->gcd());
+print_r("\n");
+//print_r("rat2 lcm " . $rat2->lcm());
+print_r(3 % 6);
+//
+print_r($rat3 = $rat1->add($rat2)); // Абстракция для рационального числа 99/27
+//function gcd(int $x, int $y): int
+//{
+//    while ($y != 0) {
+//        $temp = $y;
+//        $y = $x % $y;
+//        $x = $temp;
+//    }
+//    return abs($x);
+//}
 
-// function lcm(int $x, int $y): int
-// {
-//     return ($x * $y) / gcd($x, $y);
-// }
-// function makeRational($num, $denom)
-// {
-//     if ($denom < 0) {
-//         $num = -$num;
-//         $denom = -$denom;
-//     }
-//     $gcd = gcd($num, $denom);
-//     return ['num' => $num / $gcd, 'denom' => $denom / $gcd];
-// }
+//function lcm(int $x, int $y): int
+//{
+//    return ($x * $y) / gcd($x, $y);
+//}
 
-// function getNum($x)
-// {
-//     return $x['num'];
-// }
-// function getDenom($x)
-// {
-//     return $x['denom'];
-// }
 
-// function ratToString($x)
-// {
-//     return getNum($x) . '/' . getDenom($x);
-// }
+//$rat1 = new Rational(10, 3);
+//print_r($rat1->getNumer());
 
-// function add($rational1, $rational2)
-// {
-//     $commonDenom = lcm(getDenom($rational1), getDenom($rational2));
-//     $rational1['num'] = getNum($rational1) * ($commonDenom / getDenom($rational1));
-//     $rational2['num'] = getNum($rational2) * ($commonDenom / getDenom($rational2));
-//     $resultNum = $rational1['num'] + $rational2['num'];
-//     return makeRational($resultNum, $commonDenom);
-// }
-// function sub($rational1, $rational2)
-// {
-//     $commonDenom = lcm(getDenom($rational1), getDenom($rational2));
-//     $rational1['num'] = getNum($rational1) * ($commonDenom / getDenom($rational1));
-//     $rational2['num'] = getNum($rational2) * ($commonDenom / getDenom($rational2));
-//     $resultNum = $rational1['num'] - $rational2['num'];
-//     return makeRational($resultNum, $commonDenom);
-// }
-
-// $rational1 = makeRational(6,8);
-// $rational2 = makeRational(2,-9);
+//Реализуйте класс для работы с рациональными числами, включающий в себя следующие методы:
+//
+//Конструктор — принимает на вход числитель и знаменатель.
+//Метод getNumer() — возвращает числитель
+//Метод getDenom() — возвращает знаменатель
+//Сложение add() — прибавляет переданную дробь к дроби на которой был вызван метод.
+//Вычитание sub() — находит разность между дробью на которой был вызван метод и переданной дробью.
+//Нормализацию делать не надо!
+//
+//Подобные абстракции, как правило, создаются неизменяемыми. Поэтому сделайте так, чтобы методы add() и sub() возвращали новое рациональное число, а не мутировали объект.
+//
+//Примеры
+//<?php
+//
+//$rat1 = new Rational(3, 9);
+//$rat1->getNumer(); // 3
+//$rat1->getDenom(); // 9
+//
+//print_r($rat2 = new Rational(10, 3));
+////
+//print_r($rat3 = $rat1->add($rat2)); // Абстракция для рационального числа 99/27
+//$rat3->getNumer(); // 99
+//$rat3->getDenom(); // 27
+//
+//$rat4 = $rat1->sub($rat2); // Абстракция для рационального числа -81/27
+//$rat4->getNumer(); // -81
+//$rat4->getDenom(); // 27
