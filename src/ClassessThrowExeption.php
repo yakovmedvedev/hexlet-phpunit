8329521ba39baacfb73538823a9cfb272e768561
Yakov Medvedev <yakovmedvedev@gmail.com>
2025-10-07 01:16:28 +0300

    Classes - Throw Exeption

---
+++ src/ClassessThrowExeption.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Classess\Throw\Exeption;
+
+
+function json_decode($json, $assoc = false)
+{
+    $result = \json_decode($json, $assoc);
+
+    if (\json_last_error() !== \JSON_ERROR_NONE) {
+        throw new \Exception('JSON decode error: ' . \json_last_error_msg());
+    }
+
+    return $result;
+}
