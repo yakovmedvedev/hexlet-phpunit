fd47cd2ec29cf72f19bda35f84b1b6a2da32e786
Yakov Medvedev <yakovmedvedev@gmail.com>
2025-10-31 20:21:58 +0300

    Ood - Entity-Object

--- src/classes/Url.php
+++ src/classes/Url.php
@@ -1,44 +1,69 @@
 <?php
 
-
-use function PHPSTORM_META\map;
+namespace App\Classes;
 
 class Url
 {
     public $addr;
-    public $port;
+    // public $port;
     public $scheme;
     public $host;
+    public $queryParams;
     public function __construct($addr)
     {
+        $parsed = parse_url($addr);
+        print_r($parsed);
         $this->addr = $addr;
+        $this->scheme = $parsed['scheme'];
+        $this->host = $parsed['host'];
+        // $this->port = $parsed['port'];
+        parse_str($parsed['query'] ?? '', $this->queryParams);
+    }
+    public function getAddr()
+    {
+        return $this->addr;
     }
     public function getScheme():string
     {
-        return substr($this->addr, 0, 7) === 'http://' ? 'http' : 'https';
+        return $this->scheme;
     }
     public function getHostName():string
     {
-        $name = str_replace($this->getScheme() . '://', '', $this->addr);
-        $domain = '';
-        for ($i=0; $i<strlen($name); $i++) {
-
-            if ($name[$i] === '/' || $name[$i] === ':') {
-                break;
-            }
-            $domain .= $name[$i];
-        }
-        return $domain;
+        return $this->host;
+    }
+    // public function getPort():int
+    // {
+    //     return $this->port;
+    // }
+    public function getQueryParams()
+    {
+        return $this->queryParams;
+    }
+    public function getQueryParam(string $param, $default = null)
+    {
+        return $this->queryParams[$param] ?? $default;
+    }
+    public function equals(Url $url): bool
+    {
+        return $this->addr === $url->getAddr();
     }
 }
 //$addr = 'https://ru.hexlet.io/courses/php-object-oriented-design/lessons/modeling/exercise_unit';
-$protocol = new Url('http://ru.hexlet.io/courses/php-object-oriented-design/lessons/modeling/exercise_unit');
-$protocol2 = new Url('http://ru.hexlet.io/courses/php-object-oriented-design/lessons/modeling/exercise_unit');
+$protocol = new Url('http://yandex.ru:80?key=value&key2=value2');
+//$protocol2 = new Url('http://ru.hexlet.io/courses/php-object-oriented-design/lessons/modeling/exercise_unit');
 print_r($protocol);
 $protocol->getScheme();
 echo($protocol->getScheme());
 print_r("\n");
-echo($protocol2->getHostName());
+echo($protocol->getHostName());
+print_r("\n");
+// echo($protocol->getPort());
+print_r("\n");
+print_r($protocol->getQueryParams());
+print_r("\n");
+print_r($protocol->getQueryParam('key3', 'key'));
+print_r("\n");
+print_r($protocol->equals(new Url('http://yandex.ru:80?key=value&key2=value2')));
 
 //В данном упражнении вам предстоит реализовать класс Url, который позволяет извлекать из HTTP адреса,
 //представленного строкой, его части.
@@ -71,3 +96,45 @@ echo($protocol2->getHostName());
 //$url->getQueryParam('new'); // null
 //$url->equals(new Url('http://yandex.ru:80?key=value&key2=value2')); // true
 //$url->equals(new Url('http://yandex.ru:80?key=value')); // false
+
+//tutor's
+//class Url
+//{
+//    private $url;
+//    private $queryParams;
+//
+//    public function __construct($url)
+//    {
+//        $this->url = parse_url($url);
+//        $this->queryParams = [];
+//
+//        if (isset($this->url['query'])) {
+//            parse_str($this->url['query'], $this->queryParams);
+//        }
+//    }
+//
+//    public function getScheme()
+//    {
+//        return $this->url['scheme'];
+//    }
+//
+//    public function getHostName()
+//    {
+//        return $this->url['host'];
+//    }
+//
+//    public function getQueryParams()
+//    {
+//        return $this->queryParams;
+//    }
+//
+//    public function getQueryParam($key, $defaultValue = null)
+//    {
+//        return $this->queryParams[$key] ?? $defaultValue;
+//    }
+//
+//    public function equals(Url $url)
+//    {
+//        return $this == $url;
+//    }
+//}
